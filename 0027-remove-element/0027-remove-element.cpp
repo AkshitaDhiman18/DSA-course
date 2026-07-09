a151d1fb66a4e8c7e90ce8ea6db9fc785441ee17
Akshita Dhiman <dhimanakshita26@gmail.com>
2026-07-09 17:54:29 +0530

    Time: 0 ms (100%), Space: 11.6 MB (84.72%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
+        //l ----> next correct position
+        //r -----> traverse the array
         int n=nums.size();
         int l=0;
-
-        
-
         for(int r=0; r<n; r++){
-            if(nums[l] != val){
+            if(nums[l] != val){ //here this checks for val...agr val ni milta toh l ko aage badhao aur agr val miljata h toh r check krke swap kryao
                 l++;
             }else if(nums[r] != val){
                 swap(nums[l], nums[r]);
@@ -17,3 +16,4 @@ public:
         return l;
     }
 };
+
