cece6dc140b5f63c0a96f7e9ed0a17b4ffb0933d
Akshita Dhiman <dhimanakshita26@gmail.com>
2026-07-10 14:01:02 +0530

    Time: 11 ms (60.3%), Space: 14.2 MB (9.69%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -8,29 +8,28 @@ public:
 
         sort(nums.begin(), nums.end());
 
-        for(int i=0; i<n-2; i++){
-            if(i>0 && nums[i]==nums[i-1]) continue;
+        for(int i=0; i<n-2; i++){ //fixed pointer
 
-            int j=i+1;
-            int k=n-1;
+            int j=i+1; //left pointer
+            int k=n-1;  //right pointer
             while(j<k){
                 int sum= nums[i]+nums[j]+nums[k];
                 if(sum == target){
-                    return sum;
+                    return sum;  //return the exact sum if exists
                 }else{
                     int diff= abs(sum-target);
                     if(diff<closest){
                         closest=diff;
                         final_sum=sum;
                     }
-                    if(sum<target){
+                    if(sum<target){  //sum less than target so move left pointer in hope to find either the target or closest sum
                         j++;
-                    }else if(sum>target){
+                    }else if(sum>target){   //sum more than target so move right pointer in hope to find either the target or closest sum
                         k--;
                     }
                 }
             }
         }
-        return final_sum;
+        return final_sum; //return the final sum that is closest to target
     }
 };
