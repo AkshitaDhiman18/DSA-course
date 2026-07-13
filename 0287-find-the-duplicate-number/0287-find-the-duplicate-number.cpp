2d8f299582f0392c78e8ddd2dd5b7f50a9a3eab9
Akshita Dhiman <dhimanakshita26@gmail.com>
2026-07-13 19:16:35 +0530

    Time: 120 ms (5.33%), Space: 104.8 MB (6.46%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,12 +1,28 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+
+        //Approach 1: Solution using sorting but this is not optimal ans because its already mention in the constraints that array should not be modified
+        /*sort(nums.begin(), nums.end());
         int n=nums.size();
 
         for(int i=0; i<n-1; i++){
             if(nums[i] == nums[i+1]){
                 return nums[i];
+            }
+        }
+        return -1;*/
+
+        //Approach 2: using extra space (map)
+        unordered_map<int, int> freq;
+
+        for(int i: nums){
+            freq[i]++;
+        }
+
+        for(auto it: freq){
+            if(it.second > 1){
+                return it.first;
             }
         }
         return -1;
