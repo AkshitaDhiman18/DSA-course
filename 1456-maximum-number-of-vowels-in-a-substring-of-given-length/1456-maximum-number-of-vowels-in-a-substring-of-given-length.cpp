68b262bffaa511eb0a82133edce13c71185aaab1
Akshita Dhiman <dhimanakshita26@gmail.com>
2026-07-16 18:03:19 +0530

    Time: 6 ms (59.17%), Space: 13.1 MB (90.69%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -1,34 +1,35 @@
 class Solution {
 public:
-    int maxVowels(string s, int k) {
 
-        int n= s.size();
+    bool isvowel(char x){
+        return x=='a' || x=='e' || x=='i' || x=='o' || x=='u';
+    }
+
+    int maxVowels(string s, int k) {
+        int n=s.size();
         int l=0;
-        unordered_map<char, int> mp;
-        int max_vowel=0;
         int count=0;
+        int max_vowel=0;
 
         for(int r=0; r<n; r++){
-            mp[s[r]]++;
+            
+            if(isvowel(s[r])){
+                count++;
+            }
 
             if((r-l+1) > k){
-                mp[s[l]]--;
-
-                if(mp[s[l]] == 0){
-                    mp.erase(s[l]);
+                if(isvowel(s[l])){
+                    count--;
                 }
                 l++;
             }
+
             if((r-l+1) == k){
-                for(auto it: mp){
-                    if(it.first == 'a' || it.first == 'e' || it.first == 'i' || it.first == 'o' || it.first == 'u'){
-                        count+=it.second;
-                    }
-                }
-                max_vowel= max(max_vowel, count);
-                count=0;
+                max_vowel= max(count, max_vowel);
             }
         }
-        return max_vowel; 
+        return max_vowel;
     }
 };
+
+       
