5aa685777ba4eb6a025391f55e5d966e11e4c3ea
Akshita Dhiman <dhimanakshita26@gmail.com>
2026-07-17 15:37:45 +0530

    Time: 0 ms (100%), Space: 11.6 MB (84.97%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -6,9 +6,9 @@ public:
         int n=nums.size();
         int l=0;
         for(int r=0; r<n; r++){
-            if(nums[l] != val){ //here this checks for val...agr val ni milta toh l ko aage badhao aur agr val miljata h toh r check krke swap kryao
-                l++;
-            }else if(nums[r] != val){
+            //if(nums[l] != val){ //here this checks for val...agr val ni milta toh l ko aage badhao aur agr val miljata h toh r check krke swap kryao
+               // l++;
+            if(nums[r] != val){
                 swap(nums[l], nums[r]);
                 l++;
             }
