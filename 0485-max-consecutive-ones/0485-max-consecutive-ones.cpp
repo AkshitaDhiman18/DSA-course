e3168ba1a09ca7b264cf145ce99eb597ca9ab940
Akshita Dhiman <dhimanakshita26@gmail.com>
2026-07-25 12:15:20 +0530

    Time: 0 ms (100%), Space: 50.1 MB (93.98%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
     int findMaxConsecutiveOnes(vector<int>& nums) {
-
-        int n= nums.size();
+        //Approach 1: this is good optimal approach of traversing an array while maintaining the count of consecutive ones 
+        //tc= O(n) and constant space complexity
+        /*int n= nums.size();
         int r=0;
         int count=0;
         int maxno=0;
@@ -17,6 +18,25 @@ public:
             r++;
         }
         maxno= max(maxno, count);
+        return maxno;*/
+
+        //Approach 2: sliding window because we need to find the valid window that contains maximum number od consecutive ones
+        int n= nums.size();
+        int r;
+        int maxno=0;
+        int l=0;
+
+        for(r=0; r<n; r++){
+            if(!(nums[r]==1)){
+                maxno= max(maxno, (r-l));
+                l=r+1;
+            }
+        }
+        maxno= max(maxno, (r-l));
         return maxno;
     }
 };
+
+
+
+
