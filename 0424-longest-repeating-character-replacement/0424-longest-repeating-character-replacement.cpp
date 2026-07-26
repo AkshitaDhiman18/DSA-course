e4e90f457d57ce556d1aeba63a72d4e03978cf10
Akshita Dhiman <dhimanakshita26@gmail.com>
2026-07-26 12:33:24 +0530

    Time: 8 ms (37.77%), Space: 11.1 MB (7.8%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+
+      //Approach 1: Bruteforce approach
+      /*int n= s.size();
+      unordered_map<char, int> freq;
+      int max_freq=0;
+      int ans=0;
+
+      for(int start=0; start<n; start++){
+        freq.clear();
+        for(int end= start; end<n; end++){
+            freq[s[end]]++;
+            int max_freq=0;
+            for(auto it: freq){
+                max_freq= max(max_freq, it.second);
+            }
+
+            int window_length= end- start+1;
+
+            if(window_length-max_freq <= k){
+                ans= max(ans, window_length);
+            }
+        }
+      }
+      return ans;*/
+
+      int n= s.size();
+      int l_ptr=0;
+      int max_freq=0;
+      int max_len=0;
+
+      unordered_map<char, int> mp;
+
+      for(int r_ptr=0; r_ptr<n; r_ptr++){
+        mp[s[r_ptr]]++;
+
+        for(auto it: mp){
+            max_freq= max(max_freq, it.second);
+        }
+
+        while((r_ptr-l_ptr+1)-max_freq>k){
+            mp[s[l_ptr]]--;
+
+            for(auto it: mp){
+                max_freq= max(max_freq, it.second);
+            }
+            l_ptr++;
+        }
+        max_len= max(max_len, (r_ptr-l_ptr+1));
+      }
+      return max_len;
+    }
+};
