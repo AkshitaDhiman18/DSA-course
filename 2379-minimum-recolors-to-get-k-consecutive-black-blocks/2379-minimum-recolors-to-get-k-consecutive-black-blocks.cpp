7f54fbd5f5b41cf249dc2eb70f05e660ecaed110
Akshita Dhiman <dhimanakshita26@gmail.com>
2026-08-01 13:22:33 +0530

    Time: 0 ms (100%), Space: 8.5 MB (27.87%) - LeetHub

--- 2379-minimum-recolors-to-get-k-consecutive-black-blocks/2379-minimum-recolors-to-get-k-consecutive-black-blocks.cpp
+++ 2379-minimum-recolors-to-get-k-consecutive-black-blocks/2379-minimum-recolors-to-get-k-consecutive-black-blocks.cpp
@@ -6,21 +6,21 @@ public:
 
         int n= blocks.size();
         int l=0;
-        int count=0;
+        int recolor=0;
         int operations = INT_MAX;
         for(int r=0; r<n; r++){
             if(blocks[r] == 'W'){
-                count++;
+                recolor++;
             }
 
             if((r-l+1) > k){
-                if(blocks[l] == 'W') count--;
+                if(blocks[l] == 'W') recolor--;
 
                 l++;
             }
 
             if((r-l+1) == k){
-                operations= min(operations, count);
+                operations= min(operations, recolor);
             }
         }
         return operations; 
