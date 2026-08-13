2b9a48bc23ae1274c1612f33936e9b8637958502
Akshita Dhiman <dhimanakshita26@gmail.com>
2026-08-13 21:46:18 +0530

    Time: 0 ms (100%), Space: 13.2 MB (61.46%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -2,23 +2,23 @@
 class Solution {
 public:
 
-   bool ispresent(vector<int>& arr, int a){
+   /*bool ispresent(vector<int>& arr, int a){
     int n= arr.size();
     for(int i=0; i<n; i++){
         if(arr[i] == a) return true;
     }
     return false;
-   }
+   }*/
     
    
     int findKthPositive(vector<int>& arr, int k) {
-        int n= arr.size();
+        /*int n= arr.size();
         int max_positive=0;
 
        
         int count=0;
         //bruteforce approach
-        for(int a=1; ; a++){
+        /*for(int a=1; ; a++){
             bool value= ispresent(arr, a);
 
             if(value == false) count++;
@@ -26,9 +26,36 @@ public:
             if(count == k){
                 return a;
             }
-        }  
-        return -1;
+        }
+         
+        return -1;*/
+        int n = arr.size();
 
-        
+        int left = 0;
+        int right = n;
+
+        while (left < right) {
+
+            int mid = left + (right - left) / 2;
+
+            // Number of missing elements before arr[mid]
+            int missing = arr[mid] - (mid + 1);
+
+            if (missing < k) {
+                // Missing numbers abhi kam hain
+                // Right side jaana hai
+                left = mid + 1;
+            }
+            else {
+                // K missing numbers mil gaye
+                // Ho sakta hai answer left side mein ho
+                right = mid;
+            }
+        }
+
+        // left = first index where missing >= k
+        return left + k;
     }
 };
+
+  
