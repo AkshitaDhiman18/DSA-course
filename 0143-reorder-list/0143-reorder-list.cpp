e4d8c8cd4683f0a2b53f7bb1a804f1ed2b396539
Akshita Dhiman <dhimanakshita26@gmail.com>
2026-08-22 14:25:38 +0530

    Time: 0 ms (100%), Space: 24.1 MB (6.29%) - LeetHub

---
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -0,0 +1,83 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+
+   int get_len(ListNode* head){
+    int len=0;
+    while(head != nullptr){
+        len++;
+        head= head->next;
+    }
+    return len;
+   }
+
+
+    void reorderList(ListNode* head) {
+        int len= get_len(head);
+        //edge cases
+        if(head == nullptr || head->next == nullptr) return;
+
+        //middle nikalo
+        ListNode* slow=head;
+        ListNode* fast= head;
+        int middle_index=0;
+
+        while(fast->next != nullptr && fast->next->next != nullptr){
+            slow= slow->next;
+            middle_index+=1;
+            fast= fast->next->next;
+        }
+
+        //elements insertion in vector
+        vector<int> arr;
+        ListNode* A= slow->next;
+        while(A!= nullptr){
+            arr.push_back(A->val);
+            A= A->next;
+        }
+
+        //disconnect connection
+        slow->next = nullptr;
+
+        int size= arr.size();
+        int i= size-1;
+
+        //linking
+        ListNode* prev= head;
+        ListNode* curr= prev->next;
+        int index=0;
+        while(index < middle_index){
+            ListNode* forward= curr->next;
+            ListNode* new_node= new ListNode(arr[i]);
+
+            prev->next= new_node;
+            new_node->next= curr;
+
+            prev= curr;
+            curr= forward;
+
+            index++;
+            i--;
+        }
+
+        if(len % 2 == 0){
+            ListNode* lastNode= new ListNode(arr[i]);
+            prev->next= lastNode;
+            lastNode->next= nullptr;
+
+        }else{
+            prev->next= nullptr;
+        }
+
+        return;
+    }
+};
