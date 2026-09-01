0ab2d6106155ea154209f648832534923de39a84
Akshita Dhiman <dhimanakshita26@gmail.com>
2026-09-01 20:52:01 +0530

    Time: 93 ms (14.17%), Space: 18 MB (99.63%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -12,73 +12,43 @@ class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
 
-        if(lists.empty()) return nullptr;
+    if(lists.empty()) return nullptr;
+    
+    int n= lists.size();
+    if(n==1 && lists[0]==nullptr) return nullptr; 
+    if(n==1) return lists[0];
 
-        int n= lists.size();
+    ListNode* dummy = new ListNode(0);
+    dummy->next = lists[0];
 
-        if(n==1 && lists[0]==nullptr) return nullptr;
-        if(n==1) return lists[0];
+    for(int i = 1; i < lists.size(); i++) {
 
-        ListNode* temp1= lists[0];
-        ListNode* temp2= lists[1];
+        ListNode* previous = dummy;
+        ListNode* start1 = dummy->next;
+        ListNode* start2 = lists[i];
 
-        ListNode* dummy= new ListNode(0);
-        dummy->next= lists[0];
+        while(start1 != nullptr && start2 != nullptr) {
 
-        ListNode* prev= dummy;
-        while(temp1 != nullptr && temp2 != nullptr){
-            int value1= temp1->val;
-            int value2= temp2->val;
+            if(start1->val <= start2->val) {
+                previous = start1;
+                start1 = start1->next;
+            }
+            else {
+                ListNode* forward = start2->next;
 
-            if(value1 <= value2){
-                prev=temp1;
-                temp1=temp1->next;
-            }else if(value2 < value1){
-                ListNode* forward= temp2->next;
-                temp2->next= nullptr;
-                prev->next= temp2;
-                temp2->next= temp1;
+                previous->next = start2;
+                start2->next = start1;
 
-                prev= temp2;
-                temp2=forward;
+                previous = start2;
+                start2 = forward;
             }
         }
 
-        if(temp2 != nullptr){
-            prev->next= temp2;
+        if(start2 != nullptr) {
+            previous->next = start2;
         }
-
-        int i=2;
-        while(i<n){
-            ListNode* previous= dummy;
-            ListNode* start1= dummy->next;
-            ListNode* start2= lists[i];
-
-            while(start1 != nullptr && start2 != nullptr){
-                int value1= start1->val;
-                int value2= start2->val;
-                
-                if(value1 <= value2){
-                previous=start1;
-                start1=start1->next;
-            }else if(value2 < value1){
-                ListNode* forward= start2->next;
-                start2->next= nullptr;
-                previous->next= start2;
-                start2->next= start1;
-
-                previous= start2;
-                start2=forward;
-            }
-        }
-
-            if(start2 != nullptr){
-                previous->next= start2;
-            }
-            i++;
-        }
-
-        ListNode* head=dummy->next;
-        return head;
     }
+
+    return dummy->next;
+}
 };
