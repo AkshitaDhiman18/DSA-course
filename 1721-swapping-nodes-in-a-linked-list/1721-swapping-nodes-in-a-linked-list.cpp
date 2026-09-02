a2e3c64c32510d163d8887f72934143c8d0fbf32
Akshita Dhiman <dhimanakshita26@gmail.com>
2026-09-03 00:16:49 +0530

    Time: 7 ms (15.58%), Space: 185.2 MB (68.69%) - LeetHub

---
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
@@ -0,0 +1,62 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+
+    int get_len(ListNode* head){
+        int len=0;
+
+        while(head != nullptr){
+            head= head->next;
+            len++;
+        }
+        return len;
+    } 
+
+    ListNode* swapNodes(ListNode* head, int k) {
+
+        if(head == nullptr || head->next == nullptr) return head;
+        int size= get_len(head);
+
+        int i=1;
+        ListNode* dummy= new ListNode(0);
+        dummy->next= head;
+        ListNode* first= dummy->next;
+
+        //first node
+        while(i<k){
+            first= first->next;
+            i++;
+        }
+
+        //second node;
+        ListNode* slow= head;
+        ListNode* fast= head;
+
+        int a=1;
+        while(a<=k){
+            fast= fast->next;
+            a++;
+        }
+        while(fast != nullptr){
+            slow= slow->next;
+            fast= fast->next;
+        }
+
+        //slow represents second node
+
+        int temp= slow->val;
+        slow->val= first->val;
+        first->val= temp;
+
+        return dummy->next;
+    }
+};
