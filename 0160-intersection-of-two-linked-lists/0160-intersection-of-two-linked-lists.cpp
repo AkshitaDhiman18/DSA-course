8cff9bad939a2f9baf2623cba3dd6e9e761f67e3
Akshita Dhiman <dhimanakshita26@gmail.com>
2026-09-04 14:32:18 +0530

    Time: 59 ms (40.3%), Space: 24 MB (55.64%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -6,20 +6,23 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
-class Node{
-    int data;
-    Node* next;
-
-    Node(int __d){
-        this->data= __d;
-        this->next= nullptr;
-    }
-};
 
 class Solution {
 public:
+    int get_len(ListNode* head){
+        int len=0;
+        while(head != nullptr){
+            len++;
+            head= head->next;
+        }
+        return len;
+    }
+
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        unordered_set<ListNode*> stt;
+
+        //bruteforce approach:- Hum List A ke saare node addresses/pointers set mein store kar rahe hain.
+        //Then List B ko traverse karke check kar rahe hain ki koi node address already set mein hai ya nahi.
+        /*unordered_set<ListNode*> stt;
 
         ListNode* temp1= headA;
         ListNode* temp2= headB;
@@ -35,6 +38,38 @@ public:
             }
             temp2= temp2->next;
         }
-        return nullptr; 
+        return nullptr;*/
+
+        //optimal solution
+        int size1= get_len(headA);
+        int size2= get_len(headB);
+
+        int difference= abs(size1-size2);
+        if(difference > 0){
+            if(size1 > size2){
+                int i=0;
+                while(i<difference){
+                    headA= headA->next;
+                    i++;
+                }
+            } else if(size2 > size1){
+                int i=0;
+                while(i<difference){
+                    headB= headB->next;
+                    i++;
+                }
+            }
+        }
+
+        ListNode* temp1= headA;
+        ListNode* temp2= headB;
+
+        while(temp1 != nullptr){
+            if(temp1 == temp2) return temp2;
+            temp1= temp1->next;
+            temp2= temp2->next;
+        }
+        return nullptr;
+
     }
 };
