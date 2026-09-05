d41c6c8885ca4629733d1087596f9fb359beea1c
Akshita Dhiman <dhimanakshita26@gmail.com>
2026-09-05 22:36:11 +0530

    Time: 0 ms (100%), Space: 13.8 MB (14.17%) - LeetHub

---
+++ 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.cpp
@@ -0,0 +1,73 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int get_len(ListNode* head){
+        int len=0;
+        while(head != nullptr){
+            len++;
+            head= head->next;
+        }
+        return len;
+    }
+
+    vector<ListNode*> splitListToParts(ListNode* head, int k) {
+        vector<ListNode*> arr(k,nullptr);
+
+        if(head == nullptr) return arr;
+
+        ListNode* start= head;
+        if(k == 0){
+            arr[0]= start;
+            return arr;
+        }
+
+
+        int length= get_len(head);
+
+        int mandatory;
+        int remaining;
+        if(length < k){
+            mandatory = 1;
+            remaining=0;
+        }else{
+            mandatory= length / k;
+            remaining= length % k;
+        }
+        
+        int value;
+        int i=0;
+        
+        while(i<k && start != nullptr){
+            if(remaining > 0){
+                value= mandatory + 1;
+                remaining-=1;
+            }else{
+                value= mandatory;
+            }
+
+            ListNode* temp= start;
+            int j=1;
+            while(j<value){
+                temp= temp->next;
+                j++;
+            }
+
+            ListNode*forward= temp->next;
+            temp->next= nullptr;
+
+            arr[i]=start;
+            start= forward;
+            i++;
+        }
+        return arr;
+    }
+};
