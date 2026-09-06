4bbf443f36ca016bfb6c0d2f1a673682e2a07bee
Akshita Dhiman <dhimanakshita26@gmail.com>
2026-09-06 13:51:24 +0530

    Time: 3 ms (91.05%), Space: 15.1 MB (72.54%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -17,16 +17,23 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
+        //agr LL empty ho toh uski nullptr hi deep copy bnegi 
         if(head == nullptr) return nullptr;
+
+        //head ko 2 ways mein use krna hai 
         Node* preserve= head;
         Node* move= head;
 
+        //dummy node bnyege for deep copy so what after all we can return our deep copy
         Node* dummy= new Node(0);
+        //pt ko dummy pe point kryege 
         Node* pt= dummy;
+        //ek temp node bnyege jo har original node ki deep copy node hai aur initially usme head ki value dalvyege
         Node* temp= new Node(move->val);
+        //dummy ko join kredege is deep copy node temp se
         dummy->next= temp;
 
-
+        //same process ham repet krege original mnode ki deep copy node bmnuege aur join krwate rhege
         while(move->next != nullptr){
             Node* next_node= new Node(move->next->val);
             temp->next= next_node;
@@ -35,25 +42,36 @@ public:
             move= move->next;
             
         } 
+        //last mein temp ka next nullptr ko point krega
         temp->next= nullptr;
+        //pt dummy ke next ko fot further traversal for random pointer
         pt= dummy->next;
+
+        //is step tk hamne next pointer shi se set krdiye h
+        //ab bari h random pointer ki
+
+        //2 new pointer bnyege to track random index for both the original LL and deep copy LL which we had made above
         Node* move_ptr= nullptr;
         Node* random_find= nullptr;
 
-
+        //here preserve represents original LL and pt represents deep copy LL
         while(preserve != nullptr) {
+            //if random pointer null ko pint kra h
             if(preserve->random == nullptr){
                 pt->random= nullptr;
             }else{
+                //aur agr kisi valid node ko kra h toh yeh
                 Node* random_pointer= preserve->random;
                 int i=0;
                 random_find=head;
 
+                //this one for origianl LL
                 while(random_find != random_pointer){
                     i++;
                     random_find=random_find->next;
                 }
 
+                //this one for deep copy LL
                 move_ptr= dummy->next;
                 int j=0;
                 while(j<i){
@@ -63,6 +81,7 @@ public:
 
                 pt->random= move_ptr;
             }
+            //pointes movement to process new nodes
             preserve=preserve->next;
             pt= pt->next;
         }
