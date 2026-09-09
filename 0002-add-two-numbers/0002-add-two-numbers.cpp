f0aee38b29cd6feff06a9b3f1a6d91e5a8f21e3d
Akshita Dhiman <dhimanakshita26@gmail.com>
2026-09-10 00:12:36 +0530

    Time: 0 ms (100%), Space: 76.5 MB (98.51%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -10,52 +10,11 @@
  */
 class Solution {
 public:
-    int get_length(ListNode* head){
-        int len=0;
-        while(head != nullptr){
-            len++;
-            head= head->next;
-        }
-        return len;
-    }
+  
 
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        //size same karo
+       
         //addition kro
-
-        int size1= get_length(l1);
-        int size2= get_length(l2);
-        int diff= abs(size1 - size2);
-
-        if(size1 < size2){
-            ListNode* tail= l1;
-            while(tail->next != nullptr){
-                tail= tail->next;
-            }
-
-            int i=0;
-            while(i<diff){
-                ListNode* temp= new ListNode(0);
-                tail->next= temp;
-                tail= tail->next;
-                i++;
-            }
-            tail->next= nullptr;
-        }else if(size2 < size1){
-            ListNode* tail= l2;
-            while(tail->next != nullptr){
-                tail= tail->next;
-            }
-
-            int i=0;
-            while(i<diff){
-                ListNode* temp= new ListNode(0);
-                tail->next= temp;
-                tail= tail->next;
-                i++;
-            }
-            tail->next= nullptr;
-        }
 
         int carry=0;
         ListNode* dummy= new ListNode(0);
@@ -64,7 +23,7 @@ public:
         ListNode* pt1= l1;
         ListNode* pt2= l2;
 
-        while(pt1 != nullptr){
+        while(pt1 != nullptr && pt2 != nullptr){
             int a= pt1->val;
             int b= pt2->val;
             int value= a + b + carry;
@@ -77,10 +36,48 @@ public:
             pt2= pt2->next;
             prev= prev->next;
         }
+
+        if(pt1== nullptr){
+            while(pt2 != nullptr){
+            int a= pt2->val;
+            int value= a + carry;
+
+            int num= value % 10;
+            carry= value / 10;
+
+            ListNode* new_node= new ListNode(num);
+            prev->next= new_node;
+            new_node->next= pt1;
+
+            pt2= pt2->next;
+            prev= prev->next;
+        }
+
         if(carry != 0){
             ListNode* new_node= new ListNode(carry);
             prev->next= new_node;
             new_node->next= pt1;
+        }
+        return dummy->next;
+    }
+        if(pt2 == nullptr){
+            while(pt1 != nullptr){
+                int a= pt1->val;
+                int value= a + carry;
+
+                int num= value % 10;
+                carry= value / 10;
+
+                pt1->val= num;
+                pt1=pt1->next;
+                prev= prev->next;
+            }
+
+            if(carry != 0){
+                ListNode* new_node= new ListNode(carry);
+                prev->next= new_node;
+                new_node->next= pt1;
+            }
         }
 
         return dummy->next;
