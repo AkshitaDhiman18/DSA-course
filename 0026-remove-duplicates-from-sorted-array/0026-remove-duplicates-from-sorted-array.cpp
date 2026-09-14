19d893692f989100d5fd5c31a2c493a459114a1a
Akshita Dhiman <dhimanakshita26@gmail.com>
2026-09-14 23:34:21 +0530

    Time: 0 ms (100%), Space: 22.4 MB (97.86%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -2,25 +2,8 @@ class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
 
-        //approach 1: Brute force approach
-        set<int> st;
-
-        for(int i:nums){
-            st.insert(i);
-        }
-
-        int idx=0;
-        for(auto it: st){
-            nums[idx]= it;
-            idx++;
-
-        }
-        return idx;
-    }
-};
-
-//teo pointer approach hai with tc o(n) and sc is o(1)
-        /*int n=nums.size();
+//two pointer approach hai with tc o(n) and sc is o(1)
+        int n=nums.size();
 
         if(nums.empty()) return 0;
         int l=0;  //l last unique element ka index represent karta hai.
@@ -35,7 +18,7 @@ public:
         }
         return l+1;
     }
-}; */
+}; 
 
 
 //r (Read Pointer): Har element ko exactly ek baar read karta hai aur decide karta hai ki ye unique hai ya duplicate.
