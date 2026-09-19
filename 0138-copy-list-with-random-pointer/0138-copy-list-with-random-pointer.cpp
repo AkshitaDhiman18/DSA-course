2fd6bf38ad019b73fd256a883ff48bd8eb5aa0b8
Akshita Dhiman <dhimanakshita26@gmail.com>
2026-09-19 16:10:38 +0530

    Time: 5 ms (64.36%), Space: 15.1 MB (73.14%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -28,14 +28,17 @@ public:
 
         //dummy node bnyege for deep copy so what after all we can return our deep copy
         Node* dummy= new Node(0);
+
         //pt ko dummy pe point kryege 
         Node* pt= dummy;
+
         //ek temp node bnyege jo har original node ki deep copy node hai aur initially usme head ki value dalvyege
         Node* temp= new Node(move->val);
+
         //dummy ko join kredege is deep copy node temp se
         dummy->next= temp;
 
-        //same process ham repet krege original mnode ki deep copy node bmnuege aur join krwate rhege
+        //same process ham repeat krege original node ki deep copy node bmnuege aur join krwate rhege
         while(move->next != nullptr){
             Node* next_node= new Node(move->next->val);
             temp->next= next_node;
@@ -46,7 +49,8 @@ public:
         } 
         //last mein temp ka next nullptr ko point krega
         temp->next= nullptr;
-        //pt dummy ke next ko fot further traversal for random pointer
+
+        //pt dummy ke next ko for further traversal for random pointer
         pt= dummy->next;
 
         //is step tk hamne next pointer shi se set krdiye h
@@ -58,7 +62,7 @@ public:
 
         //here preserve represents original LL and pt represents deep copy LL
         while(preserve != nullptr) {
-            //if random pointer null ko pint kra h
+            //if random pointer null ko point kra h
             if(preserve->random == nullptr){
                 pt->random= nullptr;
             }else{
